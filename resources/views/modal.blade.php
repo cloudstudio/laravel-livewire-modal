0302c663c55119e97efa42889cd8924d1ede2539
Eric Lagarda <ericlagarda@gmail.com>
2025-04-08 13:28:54 +0200

    Fix text align

--- resources/views/modal.blade.php
+++ resources/views/modal.blade.php
@@ -5,7 +5,7 @@
         x-on:keydown.escape.window="show && closeModalOnEscape()" x-show="show" class="fixed inset-0 z-20 overflow-y-auto"
         style="display: none;">
 
-        <div class="flex items-end justify-center min-h-dvh px-4 pt-4 pb-10 sm:block sm:p-0">
+        <div class="flex items-end justify-center min-h-dvh px-4 pt-4 pb-10 text-center sm:block sm:p-0">
 
             <div x-show="show" x-on:click="closeModalOnClickAway()" x-transition:enter="ease-out duration-300"
                 x-transition:enter-start="opacity-0" x-transition:enter-end="opacity-100"
@@ -26,8 +26,7 @@
                     x-transition:leave-start="opacity-100 translate-y-0 sm:scale-100"
                     x-transition:leave-end="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
                     x-bind:class="modalWidth"
-                    class="inline-block w-full align-bottom bg-white rounded-lg text-left overflow-hidden shadow-xl transform
-                     transition-all sm:my-8 sm:align-middle sm:w-full"
+                    class="inline-block w-full align-bottom bg-white rounded-lg text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:w-full "
                     id="modal-container" x-trap.noscroll="show && showActiveComponent && !modalFlyout" aria-modal="true">
                 @include('laravel-livewire-modal::modal-content')
             </div>
@@ -42,7 +41,7 @@
                  x-transition:leave-end="translate-x-full"
                  x-bind:class="modalWidth"
                  class="fixed h-full md:w-[25rem] top-0 right-0 border-s overflow-y-auto overflow-x-hidden
-                  z-50 shadow-2xl modal-flyout-right bg-white dark:bg-zinc-800 border-zinc-200 dark:border-zinc-700 transform transition-all"
+                  z-50 shadow-2xl modal-flyout-right bg-white dark:bg-zinc-800 border-zinc-200 dark:border-zinc-700 transform transition-all text-left"
                  id="modal-container-right" x-trap.noscroll="show && showActiveComponent && modalFlyout && modalFlyoutPosition === 'right'" aria-modal="true">
                 @include('laravel-livewire-modal::modal-content')
             </div>
@@ -57,7 +56,7 @@
                  x-transition:leave-end="-translate-x-full"
                  x-bind:class="modalWidth"
                  class="fixed h-full md:w-[25rem] top-0 left-0 border-e overflow-y-auto overflow-x-hidden 
-                 z-50 shadow-2xl modal-flyout-left bg-white dark:bg-zinc-800 border-zinc-200 dark:border-zinc-700 transform transition-all"
+                 z-50 shadow-2xl modal-flyout-left bg-white dark:bg-zinc-800 border-zinc-200 dark:border-zinc-700 transform transition-all text-left"
                  id="modal-container-left" x-trap.noscroll="show && showActiveComponent && modalFlyout && modalFlyoutPosition === 'left'" aria-modal="true">
                 @include('laravel-livewire-modal::modal-content')
             </div>
@@ -72,7 +71,7 @@
                  x-transition:leave-end="translate-y-full"
                  x-bind:class="modalWidth"
                  class="fixed min-h-[25rem] w-screen min-w-[100vw] bottom-0 left-0 right-0 border-t overflow-y-auto overflow-x-hidden
-                  z-50 shadow-2xl modal-flyout-bottom bg-white dark:bg-zinc-800 border-zinc-200 dark:border-zinc-700 transform transition-all"
+                  z-50 shadow-2xl modal-flyout-bottom bg-white dark:bg-zinc-800 border-zinc-200 dark:border-zinc-700 transform transition-all text-left"
                  id="modal-container-bottom" x-trap.noscroll="show && showActiveComponent && modalFlyout && modalFlyoutPosition === 'bottom'" aria-modal="true">
                 @include('laravel-livewire-modal::modal-content')
             </div>
