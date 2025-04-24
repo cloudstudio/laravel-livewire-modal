8e8da9baff6833c1e299bf82c2ef88d18fa4e039
Toni <cloudstudio@Tonis-Mac-mini.local>
2025-04-24 12:11:26 +0200

    add livewire modal class

--- resources/views/modal-content.blade.php
+++ resources/views/modal-content.blade.php
@@ -1,7 +1,8 @@
 @foreach ($components as $id => $component)
     @if (Modal::shouldShowModal($component['modalAttributes'], $modalFlyout, $modalFlyoutPosition ?? null))
         <div x-show.immediate="activeComponent == '{{ $id }}'" x-ref="{{ $id }}"
-            wire:key="{{ $id }}">
+            wire:key="{{ $id }}"
+            class="livewire-modal">
             @livewire($component['name'], $component['arguments'], key($id))
         </div>
     @endif
